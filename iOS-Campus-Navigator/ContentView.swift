0ef820422843e7943c30f0fc442b72d8551471a0
Gimantha 044 <gimantha044@NIBMADMIN07s-iMac.local>
2025-02-07 12:26:00 +0530

    Intial Commit

---
+++ iOS-Campus-Navigator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS-Campus-Navigator
+//
+//  Created by Gimantha 044 on 2025-02-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
