7285a2ddb24af711a55a6e555430bdbd8ad4a8c2
Gimantha 044 <gimantha044@NIBMADMIN07s-iMac.local>
2025-02-07 12:16:14 +0530

    Initial Commit

---
+++ iOS-Campus-Navigator/iOS_Campus_NavigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Campus_NavigatorApp.swift
+//  iOS-Campus-Navigator
+//
+//  Created by Gimantha 044 on 2025-02-07.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Campus_NavigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS-Campus-NavigatorTests/iOS_Campus_NavigatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Campus_NavigatorTests.swift
+//  iOS-Campus-NavigatorTests
+//
+//  Created by Gimantha 044 on 2025-02-07.
+//
+
+import Testing
+@testable import iOS_Campus_Navigator
+
+struct iOS_Campus_NavigatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
