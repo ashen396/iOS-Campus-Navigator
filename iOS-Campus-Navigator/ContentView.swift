3eec8688b57a111263811874db8af1ae11e1cfb3
Gimantha 044 <gimantha044@NIBMADMIN07s-iMac.local>
2025-02-16 17:38:30 +0530

    Draggable View

--- iOS-Campus-Navigator/ContentView.swift
+++ iOS-Campus-Navigator/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var mouseLoc = CGSize.zero
+    @State private var offset = CGSize.zero
+    
+    var columns: [GridItem] = [GridItem(.fixed(50)), GridItem(.fixed(50))]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 80, height: 80)
+                .cornerRadius(20)
+                .offset(offset)
+                .gesture(DragGesture().onChanged({ value in
+                    self.offset = CGSize(width: value.translation.width + mouseLoc.width, height: value.translation.height + mouseLoc.height)
+                }).onEnded({ value in
+                    self.mouseLoc = CGSize(width:  value.translation.width + (mouseLoc.width - offset.width), height: value.translation.height + (mouseLoc.height - offset.height))
+                }))
         }
-        .padding()
     }
 }
 
