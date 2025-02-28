6413e1d150ec471c9050b0ba77f2d2d8bf6ca169
Gimantha 044 <gimantha044@NIBMADMIN07s-iMac.local>
2025-02-28 17:05:01 +0530

    Implemented ScheduleCards

--- iOS-Campus-Navigator/Views/Components/ScheduleCard.swift
+++ iOS-Campus-Navigator/Views/Components/ScheduleCard.swift
@@ -10,14 +10,25 @@ import SwiftUI
 
 struct ScheduleCard: View{
     
+    @State var isExpired: Bool = false
+    @State var title: String = "Study"
+    @State var time: String = "11:30 AM - 01:00 PM"
+    @State var description: String = "Study iOS"
+    @State private var colors: Array = [Color(red: 0.52, green: 0.70, blue: 0.37), Color(red: 0.85, green: 0.34, blue: 0.32), Color(red: 0.75, green: 0.31, blue: 0.29), Color(red: 0.64, green: 0.29, blue: 0.28)]
+    @State var colorIndex: Int = 0
+    
     var body: some View{
-            Color(red: 0.52, green: 0.70, blue: 0.37)
+            Color(colors[colorIndex])
             .frame(width: 350, height: 100, alignment: .center)
             .cornerRadius(10)
             .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color(white: 1, opacity: 0.8), lineWidth: 1)
+            )
+            .overlay(
                 VStack{
                     VStack{
-                        Text("11:30 AM - 01:00 PM")
+                        Text(!isExpired ? time : title)
                             .foregroundStyle(Color.white)
                             .fontWeight(.bold)
                             .font(.subheadline)
@@ -25,17 +36,40 @@ struct ScheduleCard: View{
                             .padding(.leading, 20)
                         
                         Color(red: 0, green: 0, blue: 0, opacity: 0.2)
-                            .frame(width: 320, height: 2)
+                            .frame(width: 330, height: 2)
                         
-                        Text("iOS Lecture")
-                            .foregroundStyle(Color.white)
-                            .fontWeight(.bold)
-                            .font(.title2)
-                            .frame(width: 350, height: 20, alignment: .topLeading)
-                            .padding(.leading, 20)
+                        HStack{
+                            VStack{
+                                Text(title)
+                                    .foregroundStyle(Color.white)
+                                    .fontWeight(.bold)
+                                    .font(.title2)
+                                    .frame(width: 350, height: 20, alignment: .topLeading)
+                                    .padding(.leading, 20)
+                                
+                                Text(description)
+                                    .foregroundStyle(Color.white)
+                                    .fontWeight(.light)
+                                    .font(.headline)
+                                    .frame(width: 350, height: 20, alignment: .topLeading)
+                                    .padding(.leading, 20)
+                            }.frame(width: 205, height: 20, alignment: .leading)
+                            
+                            if(!isExpired){
+                                Color(Color.blue)
+                                    .frame(width: 140, height: 30)
+                                    .cornerRadius(20)
+                                    .overlay(
+                                        Text("Get Directions")
+                                            .foregroundStyle(Color.white)
+                                            .font(.subheadline)
+                                            .fontWeight(.bold)
+                                    )
+                            }
+                        }.frame(width: 370, height: 40, alignment: .leading)
                     }
                 }
-            )
+            ).shadow(radius: 5, x: 0, y: 0)
         
     }
 }
